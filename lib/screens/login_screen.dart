535d136e9eceb41e125e75f961e81181c7f95064
brunop <brunop@kyros.com.br>
2020-03-26 00:04:10 -0300

    Ajustes tela login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -62,8 +62,96 @@ class LoginScreen extends StatelessWidget {
                 style: TextStyle(color: Colors.white, fontSize: 16),
               ),
               shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12)),
+                  borderRadius: BorderRadius.circular(15)),
             ),
+            SizedBox(height: 50),
+            Center(
+              child: Text(
+                'Esqueceu a senha?',
+                style: TextStyle(color: Colors.deepPurpleAccent, fontSize: 14),
+              ),
+            ),
+            SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Container(
+                  width: 150,
+                  child: OutlineButton(
+                    child: Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              right: 30, top: 15, bottom: 15),
+                          child: Image.asset(
+                            'images/icons/ic-google-64.png',
+                            width: 20,
+                            height: 20,
+                          ),
+                        ),
+                        Text(
+                          'Google',
+                          style: TextStyle(
+                              color: Colors.deepOrangeAccent, fontSize: 14),
+                        )
+                      ],
+                    ),
+                    onPressed: () {},
+                    borderSide: BorderSide(color: Colors.grey[100]),
+                    shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(30.0),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 150,
+                  child: OutlineButton(
+                    child: Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              right: 30, top: 15, bottom: 15),
+                          child: Image.asset(
+                            'images/icons/ic-facebook-48.png',
+                            width: 20,
+                            height: 20,
+                          ),
+                        ),
+                        Text(
+                          'Google',
+                          style:
+                              TextStyle(color: Colors.blueAccent, fontSize: 14),
+                        )
+                      ],
+                    ),
+                    onPressed: () {},
+                    borderSide: BorderSide(color: Colors.grey[100]),
+                    shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(30.0),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(right: 5),
+                  child: Text(
+                    'Não possui conta?',
+                    style: TextStyle(color: Colors.grey[400]),
+                  ),
+                ),
+                Text(
+                  'Criar conta',
+                  style: TextStyle(
+                      color: Colors.deepPurpleAccent,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            )
           ],
         ),
       ),
